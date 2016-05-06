aa68d090b8bc835a56b3526e94e71875564c51a7
LiaoKunZhi <a0956823675@gmail.com>
2016-05-06 20:42:12 +0800

    Create BS.java-2
    Insert the numbers to the node.

--- BS.java
+++ BS.java
@@ -9,4 +9,20 @@ public class BS {
 	public BS (Object o) {
 		root = new Node(o);
 	}
+	
+	//Insert the numbers to the node
+	public Node Insert (Node node, int o) {
+
+		if (node == null) {
+			return node = new Node(o);
+		}
+
+		if (o < (Integer) node.element) {
+			node.left = Insert(node.left, o);
+		}
+		else {
+			node.right = Insert(node.right, o);
+		}
+		return node;
+	}
 }
