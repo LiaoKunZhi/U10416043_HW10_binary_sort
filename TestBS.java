41d1f21dd15b360cab3b9057a5dcf5d79e09c6bc
LiaoKunZhi <a0956823675@gmail.com>
2016-05-06 20:21:44 +0800

    Create TestBS.java-3
    Call the class BS to here and get the sequence to the class, let the sequence can be judge.

--- TestBS.java
+++ TestBS.java
@@ -5,6 +5,15 @@ public class TestBS {
 
 	//Main method
 	public static void main (String args[]) {
-		int[] n = { 17, 2, 9, 3, 13, 0, 4, 30, 7, 31, 20, 1};
+
+		  int[] n = { 17, 2, 9, 3, 13, 0, 4, 30, 7, 31, 20, 1};
+
+		  BS bs = new BS(new Integer(n[0]));
+
+		  for (int t = 1; t < n.length; t++) {
+			bs.Insert(bs.root, new Integer(n[t]));
+		  }
+
+		  bs.inOrder(bs.root);
 	}
 }
