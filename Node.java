99db49d369b62d6596751d293c045ea9962b7b87
LiaoKunZhi <a0956823675@gmail.com>
2016-05-06 20:47:36 +0800

    Create Node.java-2
    Make constructors to let the class can work.

--- Node.java
+++ Node.java
@@ -6,4 +6,13 @@ public class Node {
 	public Object element;
 	public Node left;
 	public Node right;
+	
+	//No arg onstructor
+	public Node () { 
+	}
+
+	//Constructor
+	public Node (Object theElement) {
+		this(theElement, null, null);
+	}
 }
